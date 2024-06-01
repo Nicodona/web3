384e117819fcf721ac648fb2a29df400deb2d568
Nicodona <71098788+Nicodona@users.noreply.github.com>
2024-06-01 19:37:46 +0100

    Add files via upload
    deployed using web3 on ganache test blockchain

---
+++ demos/web3_py_simple_storage/SimpleStorage.sol
@@ -0,0 +1,29 @@
+// SPDX-Lincense-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+contract SimpleStorage {
+    uint256 public myNum;
+
+    struct People {
+        uint256 myNum;
+        string name;
+    }
+
+    People[] public people;
+
+    mapping(string => uint) public nameToNum;
+
+    function store(uint256 _myNum) public {
+        myNum = _myNum;
+    }
+
+    function retrieve() public view returns (uint256) {
+        return myNum + myNum;
+    }
+
+    function addPerson(string memory _name, uint _num) public {
+        people.push(People(_num, _name));
+        nameToNum[_name] = _num;
+    }
+}
---
+++ demos/web3_py_simple_storage/deploy.py
@@ -0,0 +1,69 @@
+from solcx import compile_standard, install_solc
+import json
+from web3 import Web3
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+with open("./SimpleStorage.sol", "r") as file:
+    simple_storage_file = file.read()
+    
+    # compile code
+    install_solc("0.6.0")
+    compiled_sol = compile_standard(
+        {
+        "language": "Solidity",
+        "sources": {"SimpleStorage.sol":{"content": simple_storage_file}},
+        "settings": {
+            "outputSelection": {
+                "*": {
+                    "*": ["abi", "metadata", "evm.bytecode", "evm.sourceMap"]
+                }
+            }
+        }
+        },
+        solc_version="0.6.0"
+    )
+
+
+    with open("compiled_code.json", "w") as file:
+        json.dump(compiled_sol, file)
+
+        # we are navigatiing using the json format to get the object(bytecode)
+        bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"]["bytecode"]["object"]
+
+        #get abi
+
+        abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
+
+
+# for connecting to grenache
+import os
+
+w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:7545'))
+chain_id = 1337
+my_address = '0x1e1C9d1623a7f69E088142A0aC82E3f35cfBD499'
+
+private_key =os.getenv("PRIVATE_KEY")
+# dont ever push your codes with public keys always set environment variables
+
+# create a contract in python
+
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+
+
+#Get latest transaction count
+
+nounce = w3.eth.get_transaction_count(my_address)
+
+#create a transaction
+transaction = SimpleStorage.constructor().build_transaction({"chainId":chain_id, "from": my_address, "nounce": nounce})
+
+#signed trx
+signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+print(signed_txn)
+
+#send txn
+tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+#tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
